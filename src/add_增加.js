0e98578c4ce1b262321cfc4c5074b0cdf70abf90
chen_anguo <cag2050@163.com>
2018-11-01 14:49:45 +0800

    m

--- src/add_增加.js
+++ src/add_增加.js
@@ -1,3 +1,4 @@
 let moment = require('moment')
 // 添加1天，并格式化为 YYYYMMDD
 console.log(moment().add(1, 'days').format('YYYYMMDD'))
+console.log(moment().subtract(30, 'days').format('YYYY-MM-DD'));
--- src/format_格式化.js
+++ src/format_格式化.js
@@ -1,3 +1,3 @@
 let moment = require('moment')
 // 格式化日期为：YYYYMMDD
-console.log(moment().format('YYYYMMDD'))
+console.log(moment().format('YYYY-MM-DD'))
